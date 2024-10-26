0121e21868abbab300218358b10db6e12618aa12
PuzanovArtem <puzanov522@gmail.com>
2024-10-26 10:24:57 +0300

    fix

--- src/js/modules/fullpageModule.js
+++ src/js/modules/fullpageModule.js
@@ -1,28 +1,5 @@
 import fullpage from 'fullpage.js'
 import 'fullpage.js/dist/fullpage.css'
-// export function initFullPage() {
-//   new fullpage('#fullpage', {
-//     autoScrolling: true,
-//     anchors: ['screenShot1', 'screenShot2', 'screenShot3', 'screenShot4', 'screenShot5', 'screenShot6'],
-//     navigation: false,
-//     controlArrows: false,
-//     scrollHorizontally: true,
-//     afterLoad: function (origin, destination, direction) {
-//       document.querySelectorAll('.pagination .point').forEach(point => {
-//         point.classList.remove('point-active')
-//       })
-//       document.querySelector(`.pagination a[href="#${destination.anchor}"]`).classList.add('point-active')
-//     },
-//   })
-
-//   document.querySelectorAll('.pagination .point').forEach(point => {
-//     point.addEventListener('click', function (e) {
-//       e.preventDefault()
-//       const targetSection = this.getAttribute('href').replace('#', '')
-//       fullpage_api.moveTo(targetSection)
-//     })
-//   })
-// }
 
 export function initFullPage() {
   function toggleFullPageScrolling() {
