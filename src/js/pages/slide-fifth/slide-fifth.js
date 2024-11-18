9d41d5b65049564336892fdea9953109765c498f
PuzanovArtem <puzanov522@gmail.com>
2024-11-18 19:16:40 +0200

    added slide five

--- src/js/pages/slide-fifth/slide-fifth.js
+++ src/js/pages/slide-fifth/slide-fifth.js
@@ -2,23 +2,19 @@ export function setupFifthScreenInteractions() {
   const buttons = document.querySelectorAll('.slide-fifth__main-point')
   const circles = document.querySelectorAll('.slide-fifth__circle')
 
-  // Отображаем первый круг при загрузке
-  circles[0].closest('.slide-fifth__circle-wrapper').style.display = 'block'
+  circles[0].closest('.slide-fifth__circle-wrapper').style.display = 'contents'
 
-  // Добавляем обработчики событий для кнопок
   buttons.forEach(button => {
     button.addEventListener('click', () => {
-      const number = button.classList[1].replace('slide-fifth__main-point-', '') // Получаем номер круга из класса кнопки
-      const targetCircle = document.querySelector(`[data-number-type="${number}"]`) // Находим круг по номеру кнопки
+      const number = button.classList[1].replace('slide-fifth__main-point-', '')
+      const targetCircle = document.querySelector(`[data-number-type="${number}"]`)
 
       if (targetCircle) {
-        // Скрываем все круги
         circles.forEach(circle => {
           circle.closest('.slide-fifth__circle-wrapper').style.display = 'none'
         })
 
-        // Показываем целевой круг
-        targetCircle.closest('.slide-fifth__circle-wrapper').style.display = 'block'
+        targetCircle.closest('.slide-fifth__circle-wrapper').style.display = 'contents'
       }
     })
   })
