d925e89acc64e92384b2e08dda2b13806f79bff9
PuzanovArtem <puzanov522@gmail.com>
2024-11-29 18:24:02 +0200

    fix popup

--- src/js/modules/popup.js
+++ src/js/modules/popup.js
@@ -1,97 +1,102 @@
 export function setupModals() {
-  const modalButtons = document.querySelectorAll('.openModalBtn')
-  const closeButtons = document.querySelectorAll('.close')
-  const backToHomeButton = document.querySelector('.about-popup__form-btn')
-  const links = document.querySelectorAll('.point')
+  const modalButtons = document.querySelectorAll('.openModalBtn');
+  const closeButtons = document.querySelectorAll('.close');
+  const backToHomeButton = document.querySelector('.about-popup__form-btn');
+  const links = document.querySelectorAll('.point');
 
   function openModal(modal) {
     if (modal) {
-      modal.style.display = 'block'
-      document.body.classList.add('modal-open')
-      $.scrollify.disable()
-      links.forEach(link => link.classList.add('disabled'))
+      modal.style.display = 'block';
+      document.body.classList.add('modal-open');
+      $.scrollify.disable();
+      links.forEach(link => link.classList.add('disabled'));
     }
   }
 
   function closeModal(modal) {
     if (modal) {
-      modal.style.display = 'none'
-      document.body.classList.remove('modal-open')
-      $.scrollify.enable()
-      links.forEach(link => link.classList.remove('disabled'))
+      modal.style.display = 'none';
+      document.body.classList.remove('modal-open');
+      $.scrollify.enable();
+      links.forEach(link => link.classList.remove('disabled'));
     }
   }
 
   window.addEventListener('click', event => {
     if (event.target.classList.contains('main-popup')) {
-      closeModal(event.target)
+      closeModal(event.target);
     }
-  })
+  });
 
   document.addEventListener('keydown', event => {
     if (event.key === 'Escape') {
-      const openModals = document.querySelectorAll(".main-popup[style='display: block;']")
-      openModals.forEach(openModal => closeModal(openModal))
+      const openModals = document.querySelectorAll(".main-popup[style='display: block;']");
+      openModals.forEach(openModal => closeModal(openModal));
     }
-  })
+  });
 
-  const burgerIcon = document.querySelector('.burger-icon')
-  const menuModal = document.getElementById('headerModal')
+  const burgerIcon = document.querySelector('.burger-icon');
+  const menuModal = document.getElementById('headerModal');
 
   burgerIcon.addEventListener('click', () => {
     if (menuModal.style.display !== 'block') {
-      openModal(menuModal)
+      openModal(menuModal);
     } else {
-      closeModal(menuModal)
+      closeModal(menuModal);
     }
-  })
+  });
 
-  const headerRequest = document.querySelector('.header__request')
-  const callModal = document.getElementById('orderCall')
+  const headerRequest = document.querySelector('.header__request');
+  const callModal = document.getElementById('orderCall');
 
   headerRequest.addEventListener('click', () => {
     if (callModal.style.display !== 'block') {
-      openModal(callModal)
+      openModal(callModal);
     } else {
-      closeModal(callModal)
+      closeModal(callModal);
     }
-  })
+  });
 
-  const headerPhoneBtn = document.querySelector('.header__phone')
+  const headerPhoneBtn = document.querySelector('.header__phone');
 
   headerPhoneBtn.addEventListener('click', () => {
     if (callModal.style.display !== 'block') {
-      openModal(callModal)
+      openModal(callModal);
     } else {
-      closeModal(callModal)
+      closeModal(callModal);
     }
-  })
+  });
 
   modalButtons.forEach(button => {
     button.addEventListener('click', () => {
-      const targetModalId = button.getAttribute('data-modal-target')
-      const targetModal = document.getElementById(targetModalId)
-      openModal(targetModal)
-    })
-  })
+      const targetModalId = button.getAttribute('data-modal-target');
+      const targetModal = document.getElementById(targetModalId);
+      openModal(targetModal);
+    });
+  });
 
   closeButtons.forEach(closeButton => {
     closeButton.addEventListener('click', () => {
-      const modal = closeButton.closest('.main-popup')
-      closeModal(modal)
-    })
-  })
+      const modal = closeButton.closest('.main-popup') || closeButton.closest('.gallery-modal');
+      if (modal) {
+        console.log('Closing modal:', modal);
+        closeModal(modal);
+      } else {
+        console.error('No modal found to close for:', closeButton);
+      }
+    });
+  });
 
   if (backToHomeButton) {
     backToHomeButton.addEventListener('click', () => {
-      console.error('Back to home button clicked')
-      const modal = backToHomeButton.closest('.main-popup')
+      console.error('Back to home button clicked');
+      const modal = backToHomeButton.closest('.main-popup');
       if (modal) {
-        closeModal(modal)
-        console.error('Modal closed')
+        closeModal(modal);
+        console.error('Modal closed');
       } else {
-        console.error('No modal found to close')
+        console.error('No modal found to close');
       }
-    })
+    });
   }
 }
